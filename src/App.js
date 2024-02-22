e0d9d9c5a81ffe6d17f3ad17bbc386364dd5ef06
Michle99 <sammiyonyx@gmail.com>
2024-02-22 18:04:30 -0500

    updates to ...

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/** 
 import React, { useState, useEffect } from "react";
 import "@aws-amplify/ui-react/styles.css";
 import './App.css';
@@ -168,3 +169,23 @@ const App = ({ signOut }) => {
 }
 
 export default withAuthenticator(App);
+
+**/
+
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
