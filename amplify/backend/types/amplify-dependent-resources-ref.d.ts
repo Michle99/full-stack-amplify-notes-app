ba4fc4e721e04ae3dd1213c4f91bda7c8429a16a
Michle99 <sammiyonyx@gmail.com>
2024-02-22 20:21:26 -0500

    deployed GraphQL API

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "fullstackamplifynote": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "fullstackamplifynote44ac6410": {
       "AppClientID": "string",
